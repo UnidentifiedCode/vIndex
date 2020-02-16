5c83024983b603fefff9e8e882f665299acb1ef2
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2020-02-16 22:11:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,70 +20,10 @@ client.on("ready", () => {
 client.on("message", async message => {
   
   if(message.author.bot) return;
-  if(message.content != `${prefix}countdown` || message.content != `${prefix}time`  || message.content != `${prefix}timeleft` || message.content != `${prefix}tl` || message.content != `${prefix}cd` && message.channel.id === "678038687430213652") {
-	if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return console.log("Insufficient permisions.")
-        message.delete();
-  }
   if(message.content.indexOf(prefix) !== 0) return;
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
-  if(command === "countdown" || command === "time" || command === "timeleft" || command === "tl" || command === "cd" ) {
-	let addtime = Math.floor(Math.random() * 2175984000*1000) + 31536000*1000;
-	let countdowntime = Date.now() + addtime;
-	var _second = 1000;
-        var _minute = _second * 60;
-        var _hour = _minute * 60;
-        var _day = _hour * 24;
-	var _month = _day * 30;
-	var _year = _month * 12;
-	var now = Date.now();
-	
-	const countdownEmbed = new RichEmbed()
-		.setAuthor(message.author.username + " countdown", client.user.avatarURL)
-		.setDescription("Death? There's a app for that!")
-		.setColor("#000000")
-		.setThumbnail(message.author.avatarURL)
-		.setTimestamp()
-		.setFooter(`${client.user.username} v0.0.3`, client.user.avatarURL);
-	
-	Counters.findOne({
-		userID: message.author.id
-	}, (err, res) => {
-		if(err) console.log(err);
-		
-		if(!res) {
-			const newDoc = new Counters({
-				userID: message.author.id,
-				username: message.author.username,
-				time: countdowntime
-			})
-			newDoc.save().catch(err => console.log(err));
-			
-			let distance = countdowntime - now;
-			let y = Math.floor(distance / _year);
-			let m = Math.floor((distance % _year) / _month);
-			let day = Math.floor((distance % _month) / _day);
-			let hour = Math.floor((distance % _day) / _hour);
-			let min = Math.floor((distance % _hour) / _minute);
-			let sec = Math.floor((distance % _minute) / _second);
-			countdownEmbed.addField(`\u200b`, `**${y}** Years **${m}** Months **${day}** Days \n**${hour}** Hours **${min}** Minutes **${sec}** Seconds`, true)
-			message.channel.send(countdownEmbed);
-        } else {
-			let distance = res.time - now;
-			let y = Math.floor(distance / _year);
-			let m = Math.floor((distance % _year) / _month);
-			let day = Math.floor((distance % _month) / _day);
-			let hour = Math.floor((distance % _day) / _hour);
-			let min = Math.floor((distance % _hour) / _minute);
-			let sec = Math.floor((distance % _minute) / _second);
-			countdownEmbed.addField(`\u200b`, `**${y}** Years **${m}** Months **${day}** Days \n**${hour}** Hours **${min}** Minutes **${sec}** Seconds`, true)
-			message.channel.send(countdownEmbed);
-        }
-      })
-  }
-  
-  
+   
   if(command === "botinfo" || command === "info"){
     const botInfo = new RichEmbed()
 	.setAuthor(client.user.username + "info", client.user.avatarURL)
